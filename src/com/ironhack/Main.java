aa247d7d8c4c72f288b22aafbb3706e7be63cccb
jasato89 <jasato89@gmail.com>
2022-05-18 13:25:57 +0200

    Ejercicio acabado

--- src/com/ironhack/Main.java
+++ src/com/ironhack/Main.java
@@ -3,34 +3,38 @@ package com.ironhack;
 import java.io.File;
 import java.io.FileWriter;
 import java.io.IOException;
-import java.io.Writer;
 import java.util.Scanner;
 
 public class Main {
 
     public static void main(String[] args) throws IOException {
 
+
+
         System.out.println("Hola mundo");
         System.out.printf("Buenas tardes \n Qué tal va todo\n Espero que bien\n");
+        //Ejemplo de error
         System.err.println("Esto es un error total");
-        int i = 10;
-        int x = 12;
-        int z = i + x;
-        z = suma(i, x);
 
-
+        //Creamos un objeto de la clase Scanner para leer el stream del teclado
         Scanner keyboard = new Scanner(System.in);
 
         System.out.println("Por favor introduce tu nombre:");
+
+        //Llamamos al keyboard para leer cada línea
         String name = keyboard.nextLine();
         System.out.println("Por favor, introduce tu apellido");
         String surname = keyboard.nextLine();
         System.out.println("Por favor, introduce tu edad");
         int age = keyboard.nextInt();
+
+        //Mediante un bucle podemos añadir condiciones al input
         while (age <= 0 || age > 130) {
             System.err.println("Edad incorrecta. Por favor, introduce tu edad");
             age = keyboard.nextInt();
         }
+
+
         if (age > 18) {
             System.out.println("Bienvenido " + name + " " + surname + ", tu edad es  " + age);
 
@@ -39,29 +43,31 @@ public class Main {
         }
 
 
-        FileWriter fileWriter = new FileWriter("./ejemplo.json", true);
+        FileWriter fileWriter = new FileWriter("./ejemplo.txt", true);
 
         fileWriter.write("Hola buenos días\n");
         int i = 12;
 
         fileWriter.write("Qué tal?\n");
+
+        //Debemos convertir los valores númericos a String
         fileWriter.write(String.valueOf(i));
 
         fileWriter.close();
 
         File file = new File("lista-de-la-compra.txt");
         Scanner reader = new Scanner(file);
-        String listaDeLaCompra = "";
+        StringBuilder listaDeLaCompra = new StringBuilder();
 
         while (reader.hasNextLine()) {
 
-            listaDeLaCompra += reader.nextLine();
+            listaDeLaCompra.append(reader.nextLine());
 
         }
         System.out.println(listaDeLaCompra);
 
         //SOLUCION EJERCICIO
-
+        // Open file to be written in
         FileWriter writer = new FileWriter("result.txt", true);
 
         // Open file
@@ -90,7 +96,6 @@ public class Main {
         // Close buffers
         scanner.close();
         writer.close();
-
 
     }
 
